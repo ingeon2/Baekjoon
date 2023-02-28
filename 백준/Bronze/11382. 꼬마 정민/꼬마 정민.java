48070581d07ba3d1d1ad64a79f36085f64ebda54
ingeon2 <63644383+ingeon2@users.noreply.github.com>
2023-03-01 01:45:18 +0900

    [Bronze V] Title: 꼬마 정민, Time: 120 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+        bw.write(String.valueOf(a + b + c));
+        bw.flush();
+        bw.close();
+
+    }
+
+}
