fad3aea7500c3ccbaa3e7b96d7c4e355a1ce6139
ingeon2 <63644383+ingeon2@users.noreply.github.com>
2023-03-02 20:07:56 +0900

    [Gold V] Title: 물통, Time: 136 ms, Memory: 23424 KB -BaekjoonHub

--- 백준/Gold/2251. 물통/물통.java
+++ 백준/Gold/2251. 물통/물통.java
@@ -1,97 +1,91 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    //answer, visited, recieve, send
-    static boolean[] answer;
-    static boolean[][][] visited;
-    static int[] recieve;
-    static int[] send;
+    //물보내는쪽
+    static int[] send = {0, 0, 1, 1, 2, 2};
+    //물받는쪽
+    static int[] receieve = {1, 2, 0, 2, 0, 1};
+    //abc 기준값
     static int[] standard;
-
+    //그 뭐냐 물 A가 0리터일때, C의 물의 양 기록해놓는곳
+    static boolean[] answer;
+    //방문여부
+    static boolean[][][] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
+        //ABC 세 물통 존재. 첫 두개는 비어있고 세번째 물통 가득 차 있음.
+        //다른 물통으로 붓기. (하나가 비거나, 다른 하나가 가득 찰 때까지 쏟음.)
+        //위 과정에서 첫번째 물통이 비어있을때 세번쨰 물통에 담겨있는 물의 양을 구하자.
         
-        //초깃값 satndard {abc입력} 만들기(비교대상)
+        //초깃값 대입후(불리언도 해줘)
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         standard = new int[3];
         standard[0] = Integer.parseInt(st.nextToken());
         standard[1] = Integer.parseInt(st.nextToken());
         standard[2] = Integer.parseInt(st.nextToken());
 
-        answer = new boolean[201];
         visited = new boolean[201][201][201];
-        recieve = new int[] {0, 0, 1, 1, 2, 2};
-        send =    new int[] {1, 2, 0, 2, 0, 1};
-        
-        //BFS 만들기
-        
-        //BFS 돌고 나서 for문으로 answer 순회하며 트루이면 bw에 넣기
-        BFS();
+        answer = new boolean[201];
 
-        for(int i = 0 ; i < answer.length ; i++){
-            if(answer[i] == true)bw.write(String.valueOf(i)+" ");
+        //BFS 거쳐서 answer 바뀌면
+        BFS(standard[2]);
+        //for문 돌면서 true인곳 체크하기 (자연스레 오름차순이 된다.)
+        for(int i = 0 ; i <= 200 ; i++) {
+            if(answer[i] == true) bw.write(String.valueOf(i) + " ");
         }
         bw.flush();
         bw.close();
 
     }
-    public static void BFS() {
-        //Queue<Node> 구현하고, q 에 add하고, 방문기록 남기고, 정답에 남기기
-        Queue<Node> q = new LinkedList<Node>();
-        q.add(new Node(0, 0, standard[2]));
-        answer[standard[2]] = true;
-        visited[0][0][standard[2]] = true;
-
-
-        //각각의 상태에서 진행할수 있는것 (a의 물을 b로 .......c의 물을 b로 까지 총 6가지. dy dx처럼 static변수로 만들기)
-        //while 큐 빌때까지, 노드 하나 뽑고 걔로 숫자 딴 다음에, for문 i로 돌리기
+    //BFS for문 6보다 작게 가자 (한 경우당 6가지씩 생기거든.)
+    static void BFS(int c) {
+        //큐 만들고, 00c 넣어주고, 방문 체크하고, 처음엔 무조건 a가 0이니까 c값 answer에 체크해주기
+        Queue<ABC> q = new LinkedList<>();
+        q.add(new ABC(0, 0, c));
+        answer[c] = true;
+        visited[0][0][c] = true;
+        //큐가 빌때까지
         while(!q.isEmpty()) {
-            //q에서 하나 뽑고 방문기록
-            Node first = q.poll();
+            //abc까지 같이 뽑아준 후 int[] now 만들기
+            ABC abc = q.poll();
 
-            //for문은 dy dx처럼 숫자세기. 총 6가지니까 1~6까지. recieve, send (dy, dx)
+
+            //for문으로 6보다 작게 받아주는 로직. 여기가 좀 헷갈릴듯.
             for(int i = 0 ; i < 6 ; i++) {
-                int[] next = {first.a, first.b, first.c};
-                //for 안에서 할 일이 물 옮기는것
-                //물 옮기는게 next[]에서 next[recieve[i]] += next[send[i]], next[send[i]] = 0
-                next[recieve[i]] += next[send[i]];
-                next[send[i]] = 0;
-
-                // 근데 next[recieve[i]] 이게 기준보다 크면(> standard[recieve[i]]) (물 넘칠때)
-                //next[send[i]] =  next[recieve[i]] - standard[recieve[i]], next[recieve[i]] = standard[recieve[i]]
-                if(next[recieve[i]] > standard[recieve[i]]){
-                    next[send[i]] = next[recieve[i]] - standard[recieve[i]];
-                    next[recieve[i]] = standard[recieve[i]];
+                int[] now = {abc.a, abc.b, abc.c};
+                //보내주는 친구의 전부를 받는 친구에게 주기
+                now[receieve[i]] += now[send[i]];
+                now[send[i]] = 0;
+                //근데 받은놈이 선을 넘어버렸다면 크기조정(여기가 헷갈리고 어려워!!!!)
+                if(now[receieve[i]] > standard[receieve[i]]) {
+                    now[send[i]] = now[receieve[i]] - standard[receieve[i]];
+                    now[receieve[i]] = standard[receieve[i]];
                 }
-
-                //바꿔준 후에 비지티드 넥스트a,b,c 방문 안했다면
-                if(visited[next[0]][next[1]][next[2]] == false) {
-                    //노드로서 큐에 추가해주고, 방문여부 체크하고
-                    q.add(new Node(next[0], next[1], next[2]));
-                    visited[next[0]][next[1]][next[2]] = true;
-                    //노드에서 A가 0이라면 답에 체크해주기 (답은 boolean형태로(answer[]), true 해주기, 나중에 순회하며 true인 얘들 답에 입력)
-                    if(next[0] == 0){
-                        answer[next[2]] = true;
+                
+                //여기까지 하면 크기조정 끝났으니
+                //방문 안했다면 방문해주고 q에 추가해주기, 혹시라도 a값이 0이면 값도 answer에 체크
+                if(visited[now[0]][now[1]][now[2]] == false) {
+                    visited[now[0]][now[1]][now[2]] = true;
+                    q.add(new ABC(now[0], now[1], now[2]));
+                    if(now[0] == 0) { //여기 핵심
+                        answer[now[2]] = true;
                     }
                 }
+
             }
         }
     }
 
-    //노드 abc 만들기. 생성자까지
-    static class Node{
+    //abc 클래스 만들어주기. 각각 a b c 물의 양
+    public static class ABC {
         int a;
         int b;
         int c;
 
-        public Node(int a, int b, int c){
+        public ABC (int a, int b, int c) {
             this.a = a;
             this.b = b;
             this.c = c;
