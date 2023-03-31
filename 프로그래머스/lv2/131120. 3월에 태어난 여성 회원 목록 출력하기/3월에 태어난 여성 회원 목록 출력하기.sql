dee1dd8d364edb68bf54028c29eef38ce12a73a6
ingeon2 <63644383+ingeon2@users.noreply.github.com>
2023-03-31 20:14:59 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
+++ 프로그래머스/lv2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -1,10 +1,11 @@
 -- ID, 이름, 성별, 생년월일을 조회
+-- 생년월일 잘 보기 !!!!!!!!!!!
 SELECT MEMBER_ID, MEMBER_NAME, GENDER, DATE_FORMAT(DATE_OF_BIRTH, "%Y-%m-%d") AS DATE_OF_BIRTH
 -- MEMBER_PROFILE 테이블에서
 FROM MEMBER_PROFILE
--- 생일이 3월인 여성 회원의
+-- 생일이 3월인 여성 회원의 MONTH 기억!!!!!!!!
 -- 전화번호가 NULL인 경우는 출력대상에서 제외
 -- 조건 순서 맞추기
 WHERE MONTH(DATE_OF_BIRTH) = 3 AND GENDER = "W" AND TLNO IS NOT NULL
 -- 결과는 회원ID를 기준으로 오름차순 정렬
-ORDER BY MEMBER_ID ASC;
+ORDER BY MEMBER_ID;
