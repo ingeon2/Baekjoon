a4a03aef8638b8338b73af86b69f4cd1c7a534b4
ingeon2 <63644383+ingeon2@users.noreply.github.com>
2023-04-07 09:53:02 +0900

    [unrated] Title: 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
+++ 프로그래머스/unrated/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
@@ -1,17 +1,18 @@
--- 코드를 입력하세요
+-- CAR_RENTAL_COMPANY_RENTAL_HISTORY 테이블에서 대여 시작일을 기준으로 2022년 8월부터 2022년 10월까지 총 대여 횟수가 5회 이상인 자동차들에 대해서 해당 기간 동안의 월별 자동차 ID 별 총 대여 횟수(컬럼명: RECORDS) 리스트를 출력하는 SQL문
 SELECT MONTH(START_DATE) AS "MONTH", CAR_ID, COUNT(CAR_ID) AS "RECORDS"
 FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
--- 대여 시작일을 기준으로 2022년 8월부터 2022년 10월까지
--- 2022년 8월부터 2022년 10월까지 총 대여 횟수가 5회 이상인 자동차들에 대해서
+
 WHERE (DATE_FORMAT(START_DATE, "%Y-%m") BETWEEN "2022-08" AND "2022-10")
-    AND CAR_ID IN (SELECT CAR_ID
-                  FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-                  WHERE (DATE_FORMAT(START_DATE, "%Y-%m") BETWEEN "2022-08" AND "2022-10")
-                  GROUP BY CAR_ID
-                  HAVING COUNT(CAR_ID) >= 5)
--- 월별 자동차 ID 별 
-GROUP BY MONTH, CAR_ID
+AND CAR_ID IN (SELECT CAR_ID
+              FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+              WHERE (DATE_FORMAT(START_DATE, "%Y-%m") BETWEEN "2022-08" AND "2022-10")
+              GROUP BY CAR_ID
+              HAVING COUNT(CAR_ID) >= 5)
+GROUP BY CAR_ID, MONTH
+
 -- 특정 월의 총 대여 횟수가 0인 경우에는 결과에서 제외
-HAVING RECORDS >= 0
--- 결과는 월을 기준으로 오름차순 정렬하고, 월이 같다면 자동차 ID를 기준으로 내림차순 정렬
+HAVING RECORDS > 0
+
+-- 월을 기준으로 오름차순 정렬하고, 월이 같다면 자동차 ID를 기준으로 내림차순 정렬
 ORDER BY MONTH, CAR_ID DESC;
+
