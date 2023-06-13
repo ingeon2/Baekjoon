5f33293011b1e742709bb0c471eac2e29f337aab
ingeon2 <63644383+ingeon2@users.noreply.github.com>
2023-06-13 09:30:44 +0900

    [Silver I] Title: 1, 2, 3 더하기 2, Time: 172 ms, Memory: 16428 KB -BaekjoonHub

--- 백준/Silver/12101. 1， 2， 3 더하기 2/1， 2， 3 더하기 2.java
+++ 백준/Silver/12101. 1， 2， 3 더하기 2/1， 2， 3 더하기 2.java
@@ -2,45 +2,45 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        ArrayList<String> [] A = new ArrayList[11];
+        ArrayList<String>[] answer = new ArrayList[11];
 
-        for(int i = 1 ; i <= 10 ; i++) {
-            A[i] = new ArrayList<>();
+        for(int i = 0 ; i <= 10 ; i++) {
+            answer[i] = new ArrayList<String>();
         }
 
+        answer[1].add("1");
 
-        A[1].add("1");
-        A[2].add("1+1");
-        A[2].add("2");
-        A[3].add("1+1+1");
-        A[3].add("1+2");
-        A[3].add("2+1");
-        A[3].add("3");
+        answer[2].add("1+1");
+        answer[2].add("2");
 
-        for(int i = 4 ; i <= n ; i++) {
-            for(int j = i-1 ; j >= i-3 ; j--) {
-                for(String s : A[j]) {
-                    A[i].add(String.valueOf(i-j) + "+" + s);
+        answer[3].add("1+1+1");
+        answer[3].add("1+2");
+        answer[3].add("2+1");
+        answer[3].add("3");
+
+        for(int i = 4 ; i <= 10 ; i++) {
+            for(int j = 1 ; j <= 3 ; j++) {
+                for(String s : answer[i-j]) {
+                    answer[i].add(j + "+" + s);
                 }
             }
         }
 
-        if(A[n].size() >= k) {
-            bw.write(A[n].get(k-1));
+        if(answer[n].size() >= k) {
+            bw.write(answer[n].get(k-1));
         }
         else{
             bw.write(String.valueOf(-1));
         }
-
 
         bw.flush();
         bw.close();
