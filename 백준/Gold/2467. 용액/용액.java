c7c6c65d764a84dc0e134aab08c02af9aad21b49
ingeon2 <63644383+ingeon2@users.noreply.github.com>
2023-07-31 20:46:43 +0900

    [Gold V] Title: 용액, Time: 424 ms, Memory: 29416 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        //투포인터
+
+        int N = Integer.parseInt(br.readLine());
+
+        long[] arr = new long[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i = 0 ; i < N ; i++) {
+            arr[i] = Long.parseLong(st.nextToken());
+        }
+
+        int s = 0;
+        int e = N-1;
+        int as = 0;
+        int ae = 0;
+        long min = Long.MAX_VALUE;
+
+        while(s < e) {
+            long sum = arr[s] + arr[e];
+
+            if(min > Math.abs(sum)) {
+                min = Math.abs(sum);
+                as = s;
+                ae = e;
+            }
+
+            if(sum > 0) {
+                e--;
+            }
+            else {
+                s++;
+            }
+        }
+
+        bw.write(String.valueOf(arr[as]) + " " + String.valueOf(arr[ae]));
+        bw.flush();
+        bw.close();
+
+    }
+    
+}
