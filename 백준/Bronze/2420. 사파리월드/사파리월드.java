b03804a664c3e42e678632de8f6033443a12cf63
ingeon2 <63644383+ingeon2@users.noreply.github.com>
2023-08-02 18:30:54 +0900

    [Bronze V] Title: 사파리월드, Time: 124 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long answer = Math.abs(a - b);
+        bw.write(String.valueOf(answer));
+        bw.flush();
+        bw.close();
+
+    }
+
+}
