9f9204bd76bbbe2e5d0b58dd7cde038c0ff14bad
ingeon2 <63644383+ingeon2@users.noreply.github.com>
2023-09-05 20:59:19 +0900

    [level 2] Title: 소수 찾기, Time: 2.01 ms, Memory: 66 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+class Solution {
+    static HashSet<Integer> set = new HashSet<>();
+    static boolean[] visited;
+    public int solution(String numbers) {
+        char[] nums = numbers.toCharArray();
+        
+        
+        visited = new boolean[nums.length];
+        
+        backTracking(0, nums, 0);
+        
+        int answer = 0;
+        for(int s : set) {
+            System.out.println(s);
+            if(isPrime(s)) answer++;
+        }
+        
+        return answer;
+    }
+    
+    
+    
+    
+    static void backTracking(int depth, char[] nums, int now) {
+        
+        if(depth == nums.length) {
+            set.add(now);
+            return;
+        }
+        
+        
+        if(now > 1) {
+            set.add(now);
+        }
+        
+        
+        for(int i = 0 ; i < nums.length ; i++) {
+            if(visited[i]) continue;
+            
+            visited[i] = true;
+            int next =  (now * 10) + (nums[i] - '0');
+            backTracking(depth+1, nums, next);
+            visited[i] = false;
+        }
+    }
+    
+    
+    
+    
+    static boolean isPrime(int a) {
+        if(a == 0 || a == 1) return false;
+        for(int i = 2 ; i <= (int) Math.sqrt(a) ; i++) {
+            if(a % i == 0) return false;
+        } 
+        return true;
+    }
+}
