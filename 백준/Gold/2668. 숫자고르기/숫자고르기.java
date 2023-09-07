a39e1c0038ac81b192d1c06bc409accb83faa98a
ingeon2 <63644383+ingeon2@users.noreply.github.com>
2023-09-07 19:35:24 +0900

    [Gold V] Title: 숫자고르기, Time: 132 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -0,0 +1,70 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    static ArrayList<Integer>[] A;
+    static boolean[] visited;
+
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        A = new ArrayList[101];
+
+        for(int i = 1 ; i <= 100 ; i++) {
+            A[i] = new ArrayList<>();
+        }
+
+        for(int i = 1 ; i <= N ; i++) {
+            A[i].add(Integer.parseInt(br.readLine()));
+        }
+
+        int answer = 0;
+        boolean[] check = new boolean[101];
+        for(int i = 1 ; i <= N ; i++) {
+            if(comeback(i)) {
+                answer++;
+                check[i] = true;
+            }
+        }
+
+        bw.write(String.valueOf(answer) + "\n");
+
+        for(int i = 1 ; i <= 100 ; i++) {
+            if(check[i]) bw.write(String.valueOf(i) + "\n");
+        }
+        bw.flush();
+        bw.close();
+
+    }
+
+
+    static boolean comeback(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        visited = new boolean[101];
+        int times = 0;
+
+        while(!q.isEmpty()) {
+            int cur = q.poll();
+
+            //start가 두번 나와야함
+            if(cur == start) {
+                times++;
+                if(times == 2) return true;
+            }
+
+            for(int next : A[cur]) {
+                if(visited[next]) continue;
+                visited[next] = true;
+                q.add(next);
+            }
+        }
+
+        return false;
+    }
+
+}
