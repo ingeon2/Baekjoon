a3684130cce840e33345b6812c15023a1253e1af
ingeon2 <63644383+ingeon2@users.noreply.github.com>
2023-09-07 20:20:17 +0900

    [Gold V] Title: 숫자고르기, Time: 128 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.java
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -5,6 +5,8 @@ import java.io.*;
 public class Main {
     static ArrayList<Integer>[] A;
     static boolean[] visited;
+    static ArrayList<Integer> answerList = new ArrayList<>();
+    static int[] arr;
 
     public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,24 +19,38 @@ public class Main {
         for(int i = 1 ; i <= 100 ; i++) {
             A[i] = new ArrayList<>();
         }
+        arr = new int[101];
 
         for(int i = 1 ; i <= N ; i++) {
-            A[i].add(Integer.parseInt(br.readLine()));
+            int a = Integer.parseInt(br.readLine());
+            A[i].add(a);
+            arr[i] = a;
         }
 
-        int answer = 0;
-        boolean[] check = new boolean[101];
+//        int answer = 0;
+//        boolean[] check = new boolean[101];
+//        for(int i = 1 ; i <= N ; i++) {
+//            if(comeback(i)) {
+//                answer++;
+//                check[i] = true;
+//            }
+//        }
+//
+//        bw.write(String.valueOf(answer) + "\n");
+//
+//        for(int i = 1 ; i <= 100 ; i++) {
+//            if(check[i]) bw.write(String.valueOf(i) + "\n");
+//        }
+
+        visited = new boolean[101];
         for(int i = 1 ; i <= N ; i++) {
-            if(comeback(i)) {
-                answer++;
-                check[i] = true;
-            }
+            visited[i] = true;
+            DFS(i, arr[i]);
+            visited[i] = false;
         }
-
-        bw.write(String.valueOf(answer) + "\n");
-
-        for(int i = 1 ; i <= 100 ; i++) {
-            if(check[i]) bw.write(String.valueOf(i) + "\n");
+        bw.write(String.valueOf(answerList.size()) + "\n");
+        for(int i = 0 ; i < answerList.size() ; i++) {
+            bw.write(String.valueOf(answerList.get(i)) + "\n");
         }
         bw.flush();
         bw.close();
@@ -65,6 +81,19 @@ public class Main {
         }
 
         return false;
+    }
+
+    static void DFS(int start, int next) { //next = arr[start]
+        if(start == arr[next]) {
+            answerList.add(start);
+            return;
+        }
+
+        if(!visited[arr[next]]) {
+            visited[arr[next]] = true;
+            DFS(start, arr[next]);
+            visited[arr[next]] = false;
+        }
     }
 
 }
