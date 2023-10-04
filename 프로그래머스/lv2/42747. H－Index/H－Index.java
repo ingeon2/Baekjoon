38ab61f0abc63b1ee0474bb9084de75d3f789614
ingeon2 <63644383+ingeon2@users.noreply.github.com>
2023-10-04 22:06:30 +0900

    [level 2] Title: H-Index, Time: 0.34 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.java
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -2,33 +2,13 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
-        int answer = 0;
         Arrays.sort(citations);
-        int N = citations.length;
-        
-        for(int i = 0 ; i <= citations.length ; i++) {
-            int x = idxOverH(citations, i); //i 이상 값의 최초index
-            
-            if(N-x >= i) {
-                answer = i;
-            }
-            else  {
-                break;
-            }
+        int l = citations.length;
+        int answer = 0;
+        for(int h  = 1 ; h <= l ; h++) {
+            if(citations[l - h] >= h) answer = h;
+            else break;
         }
-        
         return answer;
-    }
-    
-    static int idxOverH(int[] arr, int h) {
-        int s = 0;
-        int e = arr.length;
-        
-        while(s < e) {
-            int m = (s+e)/2;
-            if(arr[m] >= h) e = m;
-            else s = m+1;
-        }
-        return s;
     }
 }
