6da694f92273d4d99036c4458e4f51d6d95b2969
ingeon2 <63644383+ingeon2@users.noreply.github.com>
2023-10-10 22:38:47 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 264 ms, Memory: 21688 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,77 +1,63 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int[] time;
-    static int N,K;
+    static int s, e;
     static boolean[] visited;
-    public static void main(String[] args) throws IOException {
+    static int[] dist;
+    public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        time = new int[100001];
-        visited = new boolean[100001];
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        s = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
+        visited = new boolean[100001];
+        dist = new int[100001];
+
 
         for(int i = 0 ; i <= 100000 ; i++) {
-            time[i] = Integer.MAX_VALUE;
+            dist[i] = Integer.MAX_VALUE;
         }
+        dist[s] = 0;
 
+        daick();
 
-        fill_D_BFS();
-        bw.write(String.valueOf(time[K]));
+        bw.write(String.valueOf(dist[e]));
         bw.flush();
         bw.close();
-
-    }
-    static void fill_D_BFS() {
-        time[N] = 0;
-        PriorityQueue<Node> q = new PriorityQueue<>();
-        q.add(new Node(N, 0));
-
-        while(!q.isEmpty()) {
-            Node now = q.poll();
-            int now_loc = now.loc;
-            int now_time = now.time;
-            visited[now_loc] = true;
-
-            if(2*now_loc <= 100000 && visited[2*now_loc] == false) {
-                time[2*now_loc] = Math.min(now.time, time[2*now_loc]);
-                q.add(new Node(2*now_loc, time[2*now_loc]));
-            }
-
-            if(now_loc+1 <= 100000 && visited[now_loc+1] == false) {
-                time[now_loc+1] = Math.min(now.time+1, time[now_loc+1]);
-                q.add(new Node(now_loc+1, time[now_loc+1]));
-            }
-
-            if(now_loc-1 >= 0 && visited[now_loc-1] == false) {
-                time[now_loc-1] = Math.min(now.time+1, time[now_loc-1]);
-                q.add(new Node(now_loc-1, time[now_loc-1]));
-            }
-
-            if(visited[K] == true) break;
-        }
-
-
-
+        
     }
 
-    static class Node implements Comparable<Node>{
-        int loc;
-        int time;
-        public Node(int loc, int time) {
-            this.loc = loc;
-            this.time = time;
-        }
+    static void daick() {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+        pq.add(new int[] {s, 0});
 
-        public int compareTo(Node e) {
-            if(this.time > e.time) return 1;
-            else return -1;
+        while(!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int idx = cur[0];
+            if(visited[idx]) continue;
+            visited[idx] = true;
+            int l = cur[1];
+
+            if(2*idx <= 100000 && dist[2*idx] > dist[idx]) {
+                dist[2*idx] = dist[idx];
+                pq.add(new int[] {2*idx, dist[2*idx]});
+            }
+
+            if(idx+1 <= 100000 && dist[idx+1] > dist[idx]+1) {
+                dist[idx+1] = dist[idx]+1;
+                pq.add(new int[] {idx+1, dist[idx]+1});
+            }
+
+            if(idx-1 >= 0 && dist[idx-1] > dist[idx]+1) {
+                dist[idx-1] = dist[idx]+1;
+                pq.add(new int[] {idx-1, dist[idx]+1});
+            }
+
+            if(visited[e]) return;
+
         }
     }
+
 }
-//굳이 D와 Node 클래스 둘다 만든 이유 생각. (다익)
