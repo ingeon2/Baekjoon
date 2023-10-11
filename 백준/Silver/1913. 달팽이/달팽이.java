167ae6e2cfefb7638b2156ed6ee96fce3ff4d344
ingeon2 <63644383+ingeon2@users.noreply.github.com>
2023-10-11 15:03:19 +0900

    [Silver III] Title: 달팽이, Time: 668 ms, Memory: 92068 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.java
+++ 백준/Silver/1913. 달팽이/달팽이.java
@@ -34,30 +34,36 @@ public class Main {
         
     }
 
+    //어떤 수가 주어지면 해당 수의 제곱수를 배열에 채우는 문제.
+    //나는 위치를 변경할때 네 방향 다 while 문으로 해결했지만, dr과 dc를 잡고 방향 int d를 사용해서 때에 맞춰 해결해주면 된다.
+    //쭉 내려간 후 보는게 아니라, 이동할 자리 보면서(isValid) 값 넣어주면 된다.
+
+    //isValid 안에서 경계를 닿는건 마찬가지로 헀는데, 한번 방문해서 0이 아닌 값이 된 친구들을 만나는 로직을 (map[r][c] != 0)
+    //v, 즉 방문 배열로 바꿔주었다. (v[r][c])
+
+
     static void fill() {
         int x = N*N;
-        int r = 0;
-        int c = 0;
         int d = 0;
-        
+        int nr = -1;
+        int nc = 0;
+
         while(x > 0) {
-            map[r][c] = x;
-            v[r][c] = true;
-            if(x == w) {
-                a = r+1;
-                b = c+1;
+            nr += dr[d];
+            nc += dc[d];
+
+            if(isValid(nr, nc)) {
+                if(x == w) {
+                    a = nr+1;
+                    b = nc+1;
+                }
+                map[nr][nc] = x--;
+                v[nr][nc] = true;
             }
-            x--;
-
-            r += dr[d];
-            c += dc[d];
-
-            if(!isValid(r, c)) {
-                r -= dr[d];
-                c -= dc[d];
+            else {
+                nr -= dr[d];
+                nc -= dc[d];
                 d = (d+1)%4;
-                r += dr[d];
-                c += dc[d];
             }
         }
     }
