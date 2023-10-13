75e814e254fefe112f60a9a2eaca4eb6e27e3855
ingeon2 <63644383+ingeon2@users.noreply.github.com>
2023-10-13 16:59:24 +0900

    [Gold IV] Title: LCS 2, Time: 236 ms, Memory: 22480 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -0,0 +1,89 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+
+        int l1 = s1.length();
+        int l2 = s2.length();
+
+        int[][] D = new int[l2][l1];
+        //D[i][j] = s1이 j 길이만큼 s2가 i 길이만큼 존재 할 때 최장 공통 부분수열의 길이
+        //s1을 열으로, s2를 행으로 생각해보기
+
+        if(s1.indexOf(s2.substring(0,1)) != -1) {
+            for(int i = s1.indexOf(s2.substring(0, 1)) ; i < l1 ; i++) {
+                D[0][i] = 1;
+            }
+        }
+
+        if(s2.indexOf(s1.substring(0,1)) != -1) {
+            for(int j = s2.indexOf(s1.substring(0, 1)) ; j < l2 ; j++) {
+                D[j][0] = 1;
+            }
+        }
+
+        int R = -1;
+        int C = -1;
+        for(int r = 1 ; r < l2 ; r++) {
+            for(int c = 1 ; c < l1 ; c++) {
+                if(s2.charAt(r) == s1.charAt(c)) {
+                    D[r][c] = D[r-1][c-1] + 1;
+                    R = Math.max(r, R);
+                    C = Math.max(c, C);
+                }
+                else {
+                    D[r][c] = Math.max(D[r-1][c], D[r][c-1]);
+                }
+            }
+        }
+
+        int l = D[l2-1][l1-1];
+        //최장길이
+
+        int a = 0;
+        String answer = "";
+        while(true) {
+
+            if(R == -1 || C == -1) break;
+            
+            if(s2.charAt(R) == s1.charAt(C)) {
+                answer += s1.charAt(C);
+                R--;
+                C--;
+                a++;
+            }
+            else {
+                if(R == 0) C--;
+                else if(C == 0) R--;
+                else {
+                    if(D[R-1][C] > D[R][C-1]) {
+                        R--;
+                    }
+                    else {
+                        C--;
+                    }
+                }
+            }
+
+            if(a == l) break;
+        }
+
+        String b = "";
+        for(int i = answer.length()-1 ; i >= 0 ; i--) {
+            b += answer.charAt(i);
+        }
+
+        bw.write(String.valueOf(l)+"\n");
+        bw.write(b);
+
+        bw.flush();
+        bw.close();
+    }
+
+}
