13e1ff48be4bd92a67b04c61078ecf98c24c2357
ingeon2 <63644383+ingeon2@users.noreply.github.com>
2023-12-05 15:53:33 +0900

    [level unrated] Title: 각도기, Time: 0.02 ms, Memory: 70.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120829. 각도기/각도기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int angle) {
+        if(angle < 90) return 1;
+        if(angle == 90) return 2;
+        if(angle < 180) return 3;
+        return 4;
+    }
+}
